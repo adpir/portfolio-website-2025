fd1455bc302716b7b10e1c1d49e8e25b11a36a11
AlicePiar <adpir30@gmail.com>
2025-07-21 00:33:17 -0500

    add javascript function, and hamburger button, add fixed issues on css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("Hello, Welcome To My Portfolio!");
+
+// Function to toggle the visibility of the navigation menu
+console.log("Hello, Welcome To My Portfolio!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+// Function to toggle the visibility of the mobile navigation menu
+function mobileMenuToggle() {
+  var x = document.getElementById('mobileNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
